9397be0578cebc5be5fe9536862250bfec3f2eef
Minh <minhvn29@gmail.com>
2020-05-27 20:14:30 -0700

    Update styledComponents

--- client/src/components/Assets.jsx
+++ client/src/components/Assets.jsx
@@ -1,7 +1,6 @@
 
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 import {Image} from './styledComponents.jsx';
 
