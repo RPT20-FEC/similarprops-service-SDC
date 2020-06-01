9d11ab530694f12f938abf07d31f458ffcc7aceb
Minh <minhvn29@gmail.com>
2020-05-31 20:59:52 -0700

    Complete testing for Properties.jsx component

--- spec/Components/Properties.spec.js
+++ spec/Components/Properties.spec.js
@@ -3,13 +3,26 @@ import Adapter from 'enzyme-adapter-react-16';
 
 import React from 'react';
 import { shallow, mount } from 'enzyme';
+
 import Properties from '../../client/src/components/Properties.jsx';
+import Property from '../../client/src/components/Property.jsx';
+
+const sampleData = require('../sampleData.js');
 
 configure({ adapter: new Adapter() });
 
-describe('Properties Component', () => {
-  xit('renders', () => {
-    const wrapper = shallow(<Properties {...props}/>);
+describe('When the Properties Component renders...', () => {
+
+  const wrapper = mount(<Properties properties ={sampleData}/>);
+
+  it('renders to the DOM', () => {
     expect(wrapper.exists()).toBe(true);
   });
+
+  it('renders 12 property slides to the DOM', () => {
+    expect(wrapper.exists()).toBe(true);
+    expect(wrapper.containsMatchingElement(<Property/>));
+  });
+
+
 });
